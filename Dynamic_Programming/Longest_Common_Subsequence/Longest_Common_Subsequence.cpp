4d368e727f4f16a6172f574f0588065feb53f05b
Utkarsh-2577 <utkarshranjan2577@gmail.com>
2026-09-04 05:33:07 +0530

    Add C++ solution for Longest_Common_Subsequence

---
+++ Dynamic_Programming/Longest_Common_Subsequence/Longest_Common_Subsequence.cpp
@@ -0,0 +1,54 @@
+/*
+ * Problem Name: Longest_Common_Subsequence
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-09-04
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
+ 
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(a[i-1] == b[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            } else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+ 
+    int i = n, j = m;
+    vector<int> lcs;
+    while(i > 0 && j > 0){
+        if(a[i-1] == b[j-1]){
+            lcs.push_back(a[i-1]);
+            i--;
+            j--;
+        } else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        } else{
+            j--;
+        }
+    }
+    reverse(lcs.begin(), lcs.end());
+ 
+    cout << dp[n][m] << "\n";
+    for(int k = 0; k < (int)lcs.size(); k++){
+        cout << lcs[k] << (k == (int)lcs.size()-1 ? "" : " ");
+    }
+    cout << "\n";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    solve();
+}
